c3f4252a8c93c4f0f58e3607bfc2780eb7658c02
Henrik Bulskov <bulskov@ruc.dk>
2020-11-26 08:12:21 +0100

    Add project files.

---
+++ Components/Controllers/CategoriesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Components.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IDataService _dataService;
+
+        public CategoriesController(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            var categories = _dataService.GetCategories();
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            var category = _dataService.GetCategory(id);
+
+            return Ok(category);
+        }
+    }
+}
---
+++ DataLayer/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public interface IDataService
+    {
+        IList<Category> GetCategories();
+        Category GetCategory(int id);
+    }
+
+    public class DataService : IDataService
+    {
+
+
+        public IList<Category> GetCategories()
+        {
+            using var ctx = new NorthwindContex();
+            return ctx.Categories.ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            using var ctx = new NorthwindContex();
+            return ctx.Categories.Find(id);
+        }
+    }
+}
